79252139ecb1bafdba99aa3e51daf76e1edd957c
nidhi8joshi <nidhi8joshi@gmail.com>
2025-08-20 18:03:12 +0530

    sd

---
+++ salt-ui-react-boilerplate/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@salt-ds/core";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Salt UI React Boilerplate</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
